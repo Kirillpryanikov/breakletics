a1367148669411c0248bad35064622bad3978a9a
DavidAskarov1990 <happypeople.sleep@gmail.com>
2017-11-15 11:22:57 +0200

    Fix form registration

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,5 +1,5 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, Slides } from 'ionic-angular';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -8,9 +8,15 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['/register.scss', '/register1.scss', '/register2.scss', '/register3.scss'],
 })
 export class RegisterPageComponent {
+  @ViewChild('slider') slider: Slides;
+
   constructor(public navCtrl: NavController) {}
 
-  submit(f: NgForm){
+  goNext(){
+    this.slider.slideNext();
+  }
 
+  submitForm(f: NgForm) {
+    console.log(f.value)
   }
 }
