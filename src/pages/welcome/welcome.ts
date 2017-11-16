7e4c64a48c21b42d22761cce993701a39acfe4d2
SergeyVeli4ko <velichkosergej7@gmail.com>
2017-11-16 12:56:20 +0200

    welcom page push

--- src/pages/welcome/welcome.ts
+++ src/pages/welcome/welcome.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { LoginPageComponent } from  '../login/login';
+import { RegisterPageComponent } from  '../register/register';
 
 @Component({
   selector: 'page-welcome',
@@ -9,4 +11,9 @@ import { NavController } from 'ionic-angular';
 export class WelcomePageComponent {
 
   constructor(public navCtrl: NavController) {}
+  goToPage(pageName) {
+    console.log(pageName);
+    const page = (pageName === 'login')? LoginPageComponent : RegisterPageComponent;
+    this.navCtrl.push(page);
+  }
 }
