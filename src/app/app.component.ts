b4ddb4bec100aeedd8d8eeca2f488bf66d8a3386
smartit <smartit@MacBook-Pro-smartit.local>
2017-12-29 13:29:47 +0200

    asdfasdfasdfads

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -50,7 +50,7 @@ export class MyApp implements OnInit{
 
       // this.statusBar.backgroundColorByHexString('#161616');
       this.splashScreen.hide();
-      this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.PORTRAIT);
+      // this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.PORTRAIT);
 
       this.initTranslate();
 
