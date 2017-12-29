4abb32e77c0349d522a2896554fb899c94f37c34
SergeyVeli4ko <velichkosergej7@gmail.com>
2017-12-30 00:05:19 +0200

    fixing

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -4,6 +4,8 @@ import {DashboardComponent} from "../index";
 import {AccountComponent} from "../account/account.component";
 import {User} from "../../share/User";
 import {AuthorizationService} from "../../share/authorization.service";
+import {ConfigService} from "../config.service";
+import {InAppBrowser} from "@ionic-native/in-app-browser";
 
 @Component({
   selector: 'settings',
@@ -15,13 +17,16 @@ export class SettingsComponent implements OnInit {
   constructor(public navCtrl: NavController,
               private navParams: NavParams,
               private viewCtrl: ViewController,
-              private auth: AuthorizationService) {}
+              private auth: AuthorizationService,
+              private iab: InAppBrowser) {}
 
   ngOnInit(){
     this.user = this.auth.user.get();
     console.log('this.user', this.user);
   }
-
+  pay() {
+    const browser = this.iab.create(ConfigService.CONFIG.payUrl, '_system');
+  }
   goToDash() {
     this.navCtrl.setRoot(DashboardComponent);
   }
